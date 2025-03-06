dd10379cb91fa55f0738cc1150cba855185d01bf
LeonaaP76 <leona.prets@edu.fh-wien.ac.at>
2025-03-06 20:09:30 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,88 @@
+// Schwertangriff
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    sword = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . f . . . . . . . 
+        . . . . . . . f . . . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        . . . . . . f 1 f . . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        . . . . . . . f . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, frodo, 150, 0)
+    sword.lifespan = 1000
+    sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sword, enemy) {
+        info.changeScoreBy(1)
+        sword.destroy()
+        nazgul.setPosition(randint(0, 160), randint(0, 120))
+    })
+})
+// Kollision: Frodo trifft Nazgul
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+    frodo.say("Ouch!", 500)
+    nazgul.setPosition(randint(0, 160), randint(0, 120))
+    if (info.life() == 0) {
+        game.over(false)
+    }
+})
+let frodo: Sprite = null
+let nazgul: Sprite = null
+let sword: Sprite = null
+// Charaktere mit einfachen Bildern erstellen
+frodo = sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f f e 2 2 2 2 2 2 e e f . . 
+    . . f e 2 f f f f f f 2 e f . . 
+    . . f f f f e e e e f f f f . . 
+    . f f e f b f 4 4 f b f e f f . 
+    . f e e 4 1 f d d f 1 4 e e f . 
+    . . f e e d d d d d d e e f . . 
+    . . . f e e 4 4 4 4 e e f . . . 
+    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+nazgul = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . c c c c . . . . 
+    . . . . . . c c d d d d c . . . 
+    . . . . . c c c c c c d c . . . 
+    . . . . c c 4 4 4 4 d c c . . . 
+    . . . c 4 d 4 4 4 4 4 1 c . c c 
+    . . c 4 4 4 1 4 4 4 4 d 1 c 4 c 
+    . c 4 4 4 4 1 4 4 4 4 4 1 c 4 c 
+    f 4 4 4 4 4 1 4 4 4 4 4 1 4 4 f 
+    f 4 4 4 f 4 1 c c 4 4 4 1 f 4 f 
+    f 4 4 4 4 4 1 4 4 f 4 4 d f 4 f 
+    . f 4 4 4 4 1 c 4 f 4 d f f f f 
+    . . f f 4 d 4 4 f f 4 c f c . . 
+    . . . . f f 4 4 4 4 c d b c . . 
+    . . . . . . f f f f d d d c . . 
+    . . . . . . . . . . c c c . . . 
+    `, SpriteKind.Enemy)
+// Steuerung für Frodo
+controller.moveSprite(frodo, 100, 100)
+frodo.setStayInScreen(true)
+// Leben initialisieren
+info.setLife(3)
+// Level-Text
+scene.setBackgroundColor(11)
+game.splash("Willkommen in Mordor!")
+// Gegnerbewegung
+game.onUpdate(function () {
+    nazgul.follow(frodo, 25)
+})
